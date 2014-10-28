42e2efe2f9fd72f81a2a308b93d446032bdf3a73
ananogal <ana.nogal@gmail.com>
2014-10-28 18:20:41 +0000

    Added chat functionality

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.use(require('express').static('public'));
 
 app.get('/', function(request, response){
 	response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user conected');
+	socket.on('disconnect', function(){
+    console.log('user disconnected');
+	});
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+  	io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(3000, function(){
